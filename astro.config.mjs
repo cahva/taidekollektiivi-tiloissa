66450f649f5374ab3eef5b1681d5ecbad99c5795
Markku Virtanen <markku.virtanen@inderes.fi>
2024-03-16 14:09:55 +0200

    feat: whops! Astro db does not work on vercel, switch to cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/serverless";
 import svelte from "@astrojs/svelte";
 import db from "@astrojs/db";
-
 import tailwind from "@astrojs/tailwind";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: cloudflare(),
   integrations: [svelte(), db(), tailwind()]
 });
